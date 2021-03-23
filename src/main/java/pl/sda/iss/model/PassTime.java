811e0954946cdc94d2b049d1400d4907f04f03a8
Borys <boria81@gmail.com>
2021-03-23 23:36:06 +0000

    Utworzono model bazy danych dla zagadnienia z listą przelotów

---
+++ src/main/java/pl/sda/iss/model/PassTime.java
@@ -0,0 +1,23 @@
+package pl.sda.iss.model;
+
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pass_times")
+@Data
+public class PassTime {
+
+    @Id
+    private Long time;
+
+    @ManyToOne
+    private Coordinates coordinates;
+
+
+}
